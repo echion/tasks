66145a065110398a5a713a5c9af874c7c14a0e14
Jason Reimer <jareimer1@gmail.com>
2016-02-19 21:01:04 -0600

    changed structure

--- api/tasks.js
+++ api/tasks.js
@@ -1,23 +1,33 @@
+function add(req, res, next) {
+
+}
+
+function getById(req, res, next) {
+	res.json({
+		name: 'Do it!'
+	});
+}
+
+function getAll(req, res, next) {
+
+}
+
+function remove(req, res, next) {
+
+}
+
 module.exports = function(router) {
   	'use strict';
 
 	// This will handle the url calls for /users/:user_id
 	router
 		.route('/tasks/:taskId')
-		.get(function(req, res, next) {
-			res.json({
-				name: 'Do it!'
-			});
-		}) 
-		.delete(function(req, res, next) {
-		
-		});
+		.get(getById) 
+		.delete(remove);
 
 	router
 		.route('/tasks/')
 		.get(function(req, res, next) {
 			
-		}).post(function(req, res, next) {
-		
-		});
+		}).post(add);
 };
