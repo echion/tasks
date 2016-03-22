5ba492619837473786c3b251efab884d92899130
Jason Reimer <jareimer1@gmail.com>
2016-03-22 15:47:35 -0500

    added task cancellation test

---
+++ test/integration/task-cancellation-route-specs.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var dropDatabase = require('./drop-database'),
+    logger = require('../../logger'),
+    app,
+    agent;
+
+describe('task cancellation', function() {
+  var task;
+
+  before('init app', function(done) {
+    app = require('../../app');
+    done();
+  });
+
+  beforeEach('drop database', function(done) {
+    dropDatabase(done);
+  });
+
+  beforeEach('init agent', function(done) {
+    agent = request.agent(app);
+    done();
+  });
+
+  beforeEach('add test data', function(done) {
+    agent
+      .post('/tasks')
+      .send({ name: 'Testing' })
+      .expect(201)
+      .expect(function(res) {
+        task = res.body;
+      })
+      .end(function() {
+        agent
+          .post('/tasks/' + task.id + '/cancel')
+          .expect(204)
+          .end(done);
+      });
+  }); 
+
+  it('completing a cancelled task should fail', function(done) {
+    agent
+      .post('/tasks/' + task.id + '/done')
+      .expect(400)
+      .end(done);
+  });
+});
