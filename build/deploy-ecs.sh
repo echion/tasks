512b0943c9aef824d524cd6578571dd07c5b3ef2
Jason Reimer <jareimer1@gmail.com>
2016-04-07 18:29:59 -0500

    fixing deploy script

--- build/deploy-ecs.sh
+++ build/deploy-ecs.sh
@@ -5,5 +5,5 @@ sh -c "$login"
 
 docker info
 docker build -t $name .
-docker tag $name:latest $AWS_ECR_URL/$name:latest
-docker push $AWS_ECR_URL/$name:latest
+docker tag $name:latest $AWS_ECR_URL/echion:latest
+docker push $AWS_ECR_URL/echion:latest
